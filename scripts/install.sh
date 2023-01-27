2b63fb3d5a538cad93f6d70b682dce4785027d98
Lucas Boganin <39099403+Spacelocust@users.noreply.github.com>
2023-01-27 16:33:17 +0100

    fix: scripts install shell

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ DIR="${DIR:-"$HOME/.local/bin"}"
 # map different architecture variations to the available binaries
 ARCH=$(uname -m)
 case $ARCH in
-    i386|i686) ARCH=x86 ;;
+    i386|i686) ARCH=386 ;;
     armv6*) ARCH=armv6 ;;
     armv7*) ARCH=armv7 ;;
     aarch64*) ARCH=arm64 ;;
@@ -14,7 +14,7 @@ esac
 
 # prepare the download URL
 GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/Spacelocust/dahl/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-GITHUB_FILE="dahl_${GITHUB_LATEST_VERSION//v/}_$(uname -s)_${ARCH}.tar.gz"
+GITHUB_FILE="dahl_${GITHUB_LATEST_VERSION//v/}_$(uname -s | tr '[:upper:]' '[:lower:]')_${ARCH}.tar.gz"
 GITHUB_URL="https://github.com/Spacelocust/dahl/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
 
 # install/update the local binary
